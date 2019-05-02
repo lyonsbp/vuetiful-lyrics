5b85e78589626b61cd0e96d799e736e190d0c6d5
Brandon Lyons <lyonsbp@umich.edu>
2019-05-02 00:34:42 -0400

    Renamed lyric component, added content

---
+++ src/components/LyricCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card>
+    <v-card-title>
+
+    </v-card-title>
+    <v-card-text>
+
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
